89ea0bb46e003ca06b5abb03f169e9d07f93bad4
JohnYacoub <johnboshra@gmail.com>
2021-06-21 17:15:09 -0700

    url

--- frontend/static/js/api.js
+++ frontend/static/js/api.js
@@ -1,7 +1,7 @@
-const APIURL = 'http://localhost:3000/api/users'
+// const APIURL = 'http://localhost:3000/api/users'
 
 
-// const APIURL = 'https://vsmedia-mockup.herokuapp.com/api/users'
+const APIURL = 'https://vsmedia-mockup.herokuapp.com/api/users'
 export const getUsersList = async () => {
   try {
     const res = await fetch(APIURL);
